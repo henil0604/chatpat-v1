319149a907161d786985aeb7a6bdf4880f780a6e
Henil Malaviya <henilmalaviya06@gmail.com>
2023-05-21 10:12:35 +0530

    hotfix: no need to add 'light' class while setting light mode

--- src/utils/setDarkMode.ts
+++ src/utils/setDarkMode.ts
@@ -3,14 +3,12 @@ export default function setDarkMode(value: boolean) {
     if (!html) return value;
     // setting dark mode
     if (value) {
-        html.classList.remove("light");
         html.classList.add("dark");
     }
 
     // setting light mode
     if (!value) {
         html.classList.remove("dark");
-        html.classList.add("light");
     }
 
     return value;
